b6247f10d86609a831e290c6a37296ea417450a4
Matthew <mvttheo@outlook.com>
2024-10-30 21:45:09 -0600

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
+import Header from './components/header/Header.vue'
 </script>
 
 <template>
-
-
+  <main>
+    <Header />
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,1 +1,16 @@
-<!-- header -->
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<template>
+  <header class="bg-blue-600 text-white p-4 flex justify-between items-center">
+    <div>
+      <img src="/logo.svg" class="logo" alt="logo" />
+    </div>
+    <div>
+      <!-- button -->
+    </div>
+  </header>
+</template>
