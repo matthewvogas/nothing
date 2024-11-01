145005e69475942bdc42f6df802b0c27b3b6132f
Matthew <mvttheo@outlook.com>
2024-10-31 22:37:46 -0600

    feat: header layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import Header from './components/header/Header.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <main>
-    <Header />
-  </main>
+  <Header />
+  <router-view />
 </template>
 
 <style scoped></style>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,16 +1,12 @@
-<script>
-export default {
-  name: 'Header',
-}
-</script>
-
 <template>
-  <header class="bg-blue-600 text-white p-4 flex justify-between items-center">
-    <div>
-      <img src="/logo.svg" class="logo" alt="logo" />
-    </div>
-    <div>
-      <!-- button -->
+  <header class="w-full flex justify-center bg-white/5 backdrop-blur-sm">
+    <div class="w-full p-5 max-w-screen-xl-up flex justify-between items-center">
+      <div>
+        <img src="/logo.svg" class="logo" alt="logo" />
+      </div>
+      <div>
+        <!-- button component -->
+      </div>
     </div>
   </header>
 </template>
